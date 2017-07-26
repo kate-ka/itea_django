e01f025ec0e003d447f492fe41cdd067eb9572f6
Катерина Уполовнікова <katedreamhealer@gmail.com>
2017-07-26 10:25:37 +0300

    removed garbage

--- project/settings.py
+++ project/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import sys
-reload(sys)
-sys.setdefaultencoding("ISO-8859-1")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
