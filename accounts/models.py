290183991c0b7e1dd65e504b19ea46465a42760a
Sasha1152 <saxin@ukr.net>
2017-07-28 16:22:48 +0300

    redone branch Users

--- accounts/models.py
+++ accounts/models.py
@@ -8,17 +8,38 @@ from django.db import models
 
 class User(AbstractUser):
 
-    GENDER_CHOICES = (('UN', 'unknown'), ('MA', "male"), ('FE', "female"))
+    GENDER_CHOICES = (('unknown', 'unknown'), ('male', "male"), ('female', "female"))
 
-    mobile_number = models.CharField(max_length=20, blank=True)
+    # def default_username(user):
+    #     return 'user#{}'.format(user.id)
+
+    # FIXME Sasha Shmigelskii: create write default name look like "user#1, user#2, etc.
+    # username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(default='00', max_length=256, blank=True)
+    mobile_number = models.CharField(max_length=13, blank=True)  # +380671111111 - 13 signs
     home_address = models.CharField(max_length=100, blank=True)
-    photo = models.ImageField(upload_to='accounts/photo', blank=True)
+    photo = models.ImageField(upload_to='accounts/photo',
+                              height_field=120,
+                              width_field=160,
+                              blank=True)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default='')
 
     class Meta:
         ordering = ['id']
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
 
     def __unicode__(self):
-        return '{} {}'.format(self.first_name, self.last_name)
+        return '{}#{}'.format(self.username, self.id)
+
+    @staticmethod
+    def get_all_users():
+        return User.objects.all()
+
+    @staticmethod
+    def get_user_by_name(name):
+        return User.objects.filter(name=name).first()
+
+    def deactivation_user(self):
+        self.is_active=False
+        self.save()
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-
+from django.http import HttpResponse, HttpResponseNotFound
+import datetime
 from .models import User
 
 
@@ -12,9 +13,28 @@ class UsersView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(UsersView, self).get_context_data(**kwargs)
-        context['users'] = User.objects.all()
+        context['users'] = User.get_all_users()
         return context
 
-    @staticmethod
-    def user_list(self, request):
-        return render(request, 'apps/accounts/users.html', {})
+    def current_datetime(request):
+        now = datetime.datetime.now()
+        html = "<html><body>It is now %s.</body></html>" % now
+        return HttpResponse(html)
+
+
+class UserRegisterView(TemplateView):
+    template_name = "registration.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(UserRegisterView, self).get_context_data(**kwargs)
+        context['registration'] = User.get_all_users()
+        return context
+
+
+class UserLoginView(TemplateView):
+    template_name = "login.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(UserLoginView, self).get_context_data(**kwargs)
+        context['login'] = User.get_all_users()
+        return context
